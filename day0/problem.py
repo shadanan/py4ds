6cf7c75db7d15630d935f8a94591916a85599160
Shad Sharma <shadanan@gmail.com>
2025-02-23 07:18:15 +0000

    Project description and day0 instructions

---
+++ day0/problem.py
@@ -0,0 +1,2 @@
+def sum_of_two_values(val1: int, val2: int) -> int:
+    pass
---
+++ day0/tests.py
@@ -0,0 +1,14 @@
+import os
+
+if "PY4DS_PYTEST" in os.environ:
+    from solution import sum_of_two_values
+else:
+    from problem import sum_of_two_values
+
+
+def test_1_plus_1_is_2():
+    assert sum_of_two_values(1, 1) == 2
+
+
+def test_adding_negative_numbers():
+    assert sum_of_two_values(-2, -3) == -5
